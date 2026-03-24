93eaa8458ebd301acc74c9b974269f098217a2c1
Yanbear91 <yannissh91@gmail.com>
2026-03-25 00:30:23 +0100

    Fix sitemap and robots.txt URLs to use www subdomain for SEO

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://surgewebstudio.com'
+  const baseUrl = 'https://www.surgewebstudio.com'
 
   return [
     {
