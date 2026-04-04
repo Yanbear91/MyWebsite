8233795c3567b165a70f29fb1a770c340145229a
Yanbear91 <yannissh91@gmail.com>
2026-04-04 22:46:50 +0200

    Add www redirect to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // @ts-ignore
+  allowedDevOrigins: ['localhost', '127.0.0.1', '192.168.1.213'],
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'surgewebstudio.com' }],
+        destination: 'https://www.surgewebstudio.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
+
+
